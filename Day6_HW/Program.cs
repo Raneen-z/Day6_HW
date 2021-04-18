9277f68c84f65326b46ce8cef996b8b48e377497
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-04-19 02:21:12 +0300

    Upload card builder solution

---
+++ Day6_HW/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Day6_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //issuing Card Builder
+            DrivingLicense dl = new DrivingLicense();
+            string card=dl.setName("Raneen Ayman Alzafarani")
+                .setIDNumber(1023424242).setVer(1)
+                .setISS()
+                .setEXP()
+                .setCAT("Private")
+                .setNational("Saudi")
+                .get();
+
+            Console.WriteLine(card);
+        }
+    }
+
+    class DrivingLicense
+    {
+        private string card;
+        public DrivingLicense()
+        {
+            this.card = "--------Driving License--------\n";
+        }
+        public DrivingLicense setName(string value)
+        {
+            this.card += "Name:  " + value;
+            return this;
+        }
+        public DrivingLicense setIDNumber(int value)
+        {
+            this.card += "\nID: " + value;
+            return this;
+        }
+        public DrivingLicense setVer(int value)
+        {
+            this.card += "         VER: "+value;
+            return this;
+        }
+
+        public DrivingLicense setISS()
+        {
+            this.card += "\nISS:               " + DateTime.Now.ToString("MM/dd/yyyy");
+            return this;
+        }
+
+        public DrivingLicense setEXP()
+        {
+            this.card += "\nEXP:               " + DateTime.Now.AddYears(10).ToString("MM/dd/yyyy");
+            return this;
+        }
+
+        public DrivingLicense setCAT(string value)
+        {
+            this.card += "\nCAT: " + value;
+            return this;
+        }
+
+        public DrivingLicense setNational(string value)
+        {
+            this.card += "       NAL: " + value;
+            return this;
+        }
+
+public string get()
+        {
+            return this.card;
+        }
+    }
+}
